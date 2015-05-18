4331361e6f00c5b8736ee783b3269a6a6298fdbf
Zack Rock <zacharyrock@hotmail.com>
2015-05-18 02:05:07 -0700

    Added beginning enemy physics and dragging. Added enemies CheckGrounded. Made enemies not collide with each other.

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,11 +5,29 @@ public class Enemy : MonoBehaviour {
 
 	public float movementSpeed = 3f;
 
-	void Start () {
-	
-	}
+	float x;
+	float y;
+
+	bool dragged = false;
+	bool grounded = false;
 	
 	void Update () {
-		transform.Translate(movementSpeed * Time.deltaTime, 0f, 0f);
+		x = Input.mousePosition.x;
+		y = Input.mousePosition.y;
+		if(!dragged)
+			transform.Translate(movementSpeed * Time.deltaTime, 0f, 0f);
+	}
+
+	void OnMouseDrag() {
+		dragged = true;
+		transform.position = Camera.main.ScreenToWorldPoint(new Vector3(x,y,10.0f));
+	}
+
+	void OnMouseUp() {
+		dragged = false;
+	}
+
+	void CheckGrounded() {
+		grounded = Physics2D.Raycast(transform.position, -Vector2.up, 2.0f, LayerMask.NameToLayer("Ground"));
 	}
 }
